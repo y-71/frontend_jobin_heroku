9b839287267d6b4c19de5ff35b6e857195233f0e
Tawfik Boujeh <tboujeh@gmail.com>
2019-09-19 22:44:35 +0200

    frontend part for heroku

---
+++ src/app/Components/notification/notification.component.ts
@@ -0,0 +1,74 @@
+import { Component, OnInit, SimpleChanges } from '@angular/core';
+import { NotificationService } from 'src/app/Services/notification.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { EnterpriseService } from 'src/app/Services/enterprise.service';
+import { JobDemandeService } from 'src/app/Services/job-demande.service';
+
+@Component({
+  selector: 'app-notification',
+  templateUrl: './notification.component.html',
+  styleUrls: ['./notification.component.scss']
+})
+export class NotificationComponent implements OnInit {
+
+  constructor(private notificationService:NotificationService,
+              private router:Router,
+              private route:ActivatedRoute,
+              private enterpriseService:EnterpriseService,
+              private jobDemandeService:JobDemandeService) { 
+                
+               }
+  loggedUser;
+  myNotification=null;
+  notifications:any[]=[];
+  
+  ngOnInit() {
+      this.loadLoggedUser();
+  }
+
+  loadLoggedUser() {
+    this.loggedUser = JSON.parse(localStorage.getItem('currentUser'));
+    console.log(this.loggedUser);
+    if (this.loggedUser) {
+      this.listenToNotification();
+      this.loadNotifications(this.loggedUser.id);
+    }
+  }
+   //get current user's notifications
+   loadNotifications(userId){
+    this.notificationService.getNotifications(userId)
+       .subscribe(notifications=>{
+         console.log("hello notification")
+         console.log(notifications);
+          this.notifications=notifications;
+       })
+}
+
+ listenToNotification(){
+   console.log("listeneing");
+     this.notificationService.eventCallback$
+        .subscribe(notif=>{
+          console.log("eated");
+          console.log(notif);
+            this.myNotification=notif;
+            this.getNotificationDetails(notif);
+        })
+ }
+
+ getNotificationDetails(notification){
+   console.log("notification changed");
+   this.myNotification=notification;
+ }
+  
+ Confirm(){
+   var id=this.myNotification.jobDemandeID;
+    this.jobDemandeService.confirmJobDemande(id)
+      .subscribe(jb=>{
+          
+          console.log("jobDemande confirmed");
+      })
+ }
+
+
+
+}
